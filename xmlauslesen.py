7afdcda5eabba2b27a0241041f217731920a120d
Chantalkle <33465110+Chantalkle@users.noreply.github.com>
2021-02-25 20:41:53 +0100

    Update xmlauslesen.py
    figure und object werden jetzt mit ihrer ID dargestellt, QSLink typen werden nun auch durch zahlen dargestellt

--- xmlauslesen.py
+++ xmlauslesen.py
@@ -6,12 +6,26 @@ trainingsdaten = []
 for child in matterportroot:
     if child.tag == "QSLINK" and child.get('figure') != "unknown" and child.get('ground') != "unknown":
         if child.get('relType') == "NTTPc":
-             trainingsdaten.append([child.get('ground'), child.get('figure'), 'NTTP'])
+             trainingsdaten.append([child.get('ground'), int(child.get('fromId')), 'NTPP'])
         elif child.get('relType') == "TPPc":
-            trainingsdaten.append([child.get('ground'), child.get('figure'), 'NPP'])
+            trainingsdaten.append([child.get('ground'), int(child.get('fromId')), 'NPP'])
         else:
-            trainingsdaten.append([child.get('figure'), child.get('ground'), child.get('relType')])
+            trainingsdaten.append([int(child.get('fromId')), child.get('ground'), child.get('relType')])
 
+
+for x in trainingsdaten:
+    for child in matterportroot:
+        if child.tag == "SPATIAL_ENTITY" and x[0] == child.get('label'):
+            x[0] = int(child.get('objectId'))
+        elif child.tag == "SPATIAL_ENTITY" and x[1] == child.get('label'):
+            x[1] = int(child.get('objectId'))
+    
+    if x[2] == "PO":
+        x[2] = 100
+    elif x[2] == "NTPP":
+        x[2] = 101
+    elif x[2] == "EC":
+        x[2] = 102
 
 sdata = open('trainingsdaten.txt', "w")
 sdata.write(str(trainingsdaten))
